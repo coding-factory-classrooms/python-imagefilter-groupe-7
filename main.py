52ca46cd38d2b0890b742521f3b5b3f082c0ee16
FlorentSalmon <florentsalmon78@gmail.com>
2020-12-16 13:43:10 +0100

    add gray filter + requirements.txt

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print('coucou')
+import sys
+import cv2
+
+args = sys.argv
+for i in range(0, len(args)):
+    arg = args[i]
+    print(arg)
+    if arg == '--color':
+        image = cv2.imread('data/img/imageTest.jpeg')
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        cv2.imwrite('data/output/newImage.jpeg', gray)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()

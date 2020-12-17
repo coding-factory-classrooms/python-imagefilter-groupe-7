a2b1cb909388ff2c5dd5cccf7d8a137517caf432
FlorentSalmon <florentsalmon78@gmail.com>
2020-12-17 11:24:04 +0100

    Ajout cli de output + input

--- filters/gaussianblur.py
+++ filters/gaussianblur.py
@@ -1,6 +1,7 @@
 import cv2
 
 def filter_blur(image):
+
     # Applique un filtre de flou
     image_blur = cv2.GaussianBlur(image, (5, 5), 5)
     # retourne l'image avec le filtre
--- main.py
+++ main.py
@@ -1,24 +1,44 @@
+import sys
 import cv2
 import os
 
 from filters import gaussianblur, grayscale, dilate
 
-input_dir = 'data/img' # Chemin dossier 'img'
-files = os.listdir(input_dir) # Retourne une liste de ce qui se trouve dans le dossier 'img'
-for f in files:
-    try:
-        file_path = f"{input_dir}/{f}" # Va cherche un element de la liste à chaque tour de boucle
-        # récupère l'image de base
-        image = cv2.imread(file_path)
 
-        #Application des filtres
+args = sys.argv # Liste des arguments
+for i in range(0, len(args)):
+    arg = args[i]
+    if arg == '-h':
+        print('HELP')
+    # OUTPUT
+    if arg == '-o':
+        path_output = args[i + 1] # Prends l'argument qui suit le '-o'
+        if os.path.exists(path_output) == False: # Vérifie que le dossier n'existe pas
+            os.makedirs(path_output) # Si il n'existe pas, le crée
 
-        image = gaussianblur.filter_blur(image)
-        image = grayscale.filter_grayscale(image)
-        image = dilate.filter_dilate(image)
+    #INPUT
+    if arg == '-i':
+        path_input = args[i + 1] # Prends l'argument qui suit le '-i'
+        if os.path.exists(path_input): # Vérifie que le dossier existe
+            input_dir = path_input  # Chemin dossier 'input'
+            files = os.listdir(input_dir)  # Retourne une liste de ce qui se trouve dans le dossier 'input'
+            for f in files:
 
-        #Enregistrer l'image filtrée dans le dossier de sortie
-        cv2.imwrite(f'data/output/{f}', image)
-    # Attrape erreur cv2
-    except cv2.error as e:
-        print(e)
+                try:
+                    file_path = f"{input_dir}/{f}" # Va cherche un element de la liste à chaque tour de boucle
+                    # récupère l'image de base
+                    image = cv2.imread(file_path)
+
+                    #Application des filtres
+
+                    image = gaussianblur.filter_blur(image)
+                    image = grayscale.filter_grayscale(image)
+                    image = dilate.filter_dilate(image)
+
+                    #Enregistrer l'image filtrée dans le dossier de sortie voulu
+                    cv2.imwrite(f'{path_output}/{f}', image)
+                # Attrape erreur cv2
+                except cv2.error as e:
+                    print(e)
+        else:
+            print('Chemin incorrect')

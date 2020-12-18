d1f6e6a0e2aeca4dee117c0d4ea9a504fee512d4
FlorentSalmon <florentsalmon78@gmail.com>
2020-12-18 09:22:43 +0100

    config-file + image.ini

---
+++ config.py
@@ -0,0 +1,9 @@
+from configparser import ConfigParser
+import os
+def apply_config(path_ini):
+    file = path_ini
+    config = ConfigParser()
+    config.read(file)
+
+    cmd = f'python main.py -i {config["general"]["input_dir"]} -o {config["general"]["output_dir"]} --filters "{config["filters"]["content"]}"'
+    os.system(cmd)
--- main.py
+++ main.py
@@ -4,30 +4,39 @@ import os
 import logger
 
 from filters import gaussianblur, grayscale, dilate
-
+import config
 
 args = sys.argv # Liste des arguments
 for i in range(0, len(args)):
-    arg = args[i]
+    try:
+        arg = args[i]
 
-    if arg == '-h':
-        print('HELP')
+        #HELP
+        if arg == '-h':
+            print('--help')
 
-    # OUTPUT
-    if arg == '-o':
-        path_output = args[i + 1] # Prends l'argument qui suit le '-o'
-        if os.path.exists(path_output) == False: # Vérifie que le dossier n'existe pas
-            os.makedirs(path_output) # Si il n'existe pas, le crée
+        #
+        if arg == '--config-file':
+            path_ini = args[i + 1]
+            config.apply_config(path_ini)
 
-    #INPUT
-    if arg == '-i':
-        path_input = args[i + 1] # Prends l'argument qui suit le '-i'
-        if os.path.exists(path_input): # Vérifie que le dossier existe
-            input_dir = path_input  # Chemin dossier 'input'
-            files = os.listdir(input_dir)  # Retourne une liste de ce qui se trouve dans le dossier 'input'
+        # OUTPUT
+        if arg == '-o':
+            path_output = args[i + 1] # Prends l'argument qui suit le '-o'
+            if os.path.exists(path_output) == False: # Vérifie que le dossier n'existe pas
+                os.makedirs(path_output) # Si il n'existe pas, le crée
 
-        else:
-            print('Chemin incorrect')
+        #INPUT
+        if arg == '-i':
+            path_input = args[i + 1] # Prends l'argument qui suit le '-i'
+            if os.path.exists(path_input): # Vérifie que le dossier existe
+                input_dir = path_input  # Chemin dossier 'input'
+                files = os.listdir(input_dir)  # Retourne une liste de ce qui se trouve dans le dossier 'input'
+
+            else:
+                print('Chemin incorrect')
+    except IndexError as e:
+        print(e)
 
     if arg == '--filters':
         for f in files:
@@ -58,8 +67,12 @@ for i in range(0, len(args)):
                 file_exit_path = f'{path_output}/{f}'
                 cv2.imwrite(file_exit_path, image)
                 logger.log_in_file(f'Save result image to image = ' + file_exit_path)
+                logger.print_log_in_console()
             except cv2.error as e:
                 print(e)
             except NameError as e:
                 print(e)
-logger.print_log_in_console()
+            except FileNotFoundError as e:
+                print(e)
+            except IndexError as e:
+                print(e)
